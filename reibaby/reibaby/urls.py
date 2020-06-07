da07e28beef8c64dcbdbe9536936687ebd368188
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-06-07 17:20:08 +0800

    added a homepage

--- reibaby/reibaby/urls.py
+++ reibaby/reibaby/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
+
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
+    path('', index, name='index')
 ]
